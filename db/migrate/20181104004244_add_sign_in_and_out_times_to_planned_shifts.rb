4c26ea95d0d0b49ab789bee7ea3089ffc7e3c1bb
Tim Davis <timhdavis@yahoo.com>
2018-11-21 13:13:32 -0800

    fix migration issue incorrect date data type
    -edited a migration to prevent the production database from encountering an error when running the migration with incorrect data type for the "sign_in_time" and "sign_out_time" columns in the planned_shifts table. Changed the column types to be the correct data type in the migration.

--- db/migrate/20181104004244_add_sign_in_and_out_times_to_planned_shifts.rb
+++ db/migrate/20181104004244_add_sign_in_and_out_times_to_planned_shifts.rb
@@ -1,6 +1,6 @@
 class AddSignInAndOutTimesToPlannedShifts < ActiveRecord::Migration[5.1]
   def change
-    add_column :planned_shifts, :sign_in_time, :date_time
-    add_column :planned_shifts, :sign_out_time, :date_time
+    add_column :planned_shifts, :sign_in_time, :datetime
+    add_column :planned_shifts, :sign_out_time, :datetime
   end
 end
