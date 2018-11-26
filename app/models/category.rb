88595752b97afe0a192ea445c0071ba69c37d997
Tim Davis <timhdavis@yahoo.com>
2018-11-26 15:51:00 -0800

    implemented all Category controller actions
    -implemented Controller actions for the Category Controller.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  # Required fields:
+  validates :name, presence: true
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181104221545) do
+ActiveRecord::Schema.define(version: 20181126195934) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
