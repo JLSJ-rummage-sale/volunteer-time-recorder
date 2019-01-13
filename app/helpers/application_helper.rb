e0054d29340e96932b940e93a1c76193660c2d47
Tim Davis <timhdavis@yahoo.com>
2019-01-13 09:54:14 -0800

    refactored add record buttons
    -refactored the "Add [Record]" buttons on the Index pages.
    --the Font Awesome plus ("+") icon is added before the button name by default.
    --using a helper method that renders a partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def render_add_record_button(btn_name, path, icon=nil)
+    render(:partial => 'layouts/buttons/add_record_button',
+      :locals => {btn_name: btn_name,
+                  path: path,
+                  icon: icon})
+  end
 end
